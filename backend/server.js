1b97f809ffd482dd4de8931f41e23b45e53fa0f8
Mark Anthony <touya1313@hotmail.com>
2024-09-23 04:00:01 -0600

    set up dotenv and mongoose in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express'); 
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connects to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // A basic route to check if API is running
 app.get('/', (req, res) => {
