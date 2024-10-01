cfe2aba308d037ec3df352c9b2f13d7407bb1622
Mark Anthony <touya1313@hotmail.com>
2024-10-01 04:45:36 -0600

    connected everything to app.jsx for now. simple

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,4 +4,16 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
 
+export default App;
