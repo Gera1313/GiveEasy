f542ce7a520a6d8c1528752ab0ada3293e4620b6
Mark Anthony <touya1313@hotmail.com>
2024-10-01 06:03:01 -0600

    got a page running. Basic for now

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,7 @@
+// src/components/Home.jsx
+const Dashboard = () => {
+    return <h1>Welcome to GiveEasy!</h1>;
+  };
+  
+  export default Dashboard;
+  
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,7 @@
+// src/components/Home.jsx
+const Login = () => {
+    return <h1>Welcome to GiveEasy!</h1>;
+  };
+  
+  export default Login;
+  
