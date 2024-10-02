0eaf7671a4fbfa0b55d014cb07bdeba68f217fe9
Mark Anthony <touya1313@hotmail.com>
2024-10-02 03:33:20 -0600

    added username and password to the home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,8 +4,11 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600 mb-6">Welcome to GiveEasy!</h1>
-      <div className="flex space-x-4">
-        <Link to="/login">
+      <input type="text" placeholder="Username" className="mt-4 p-2 border border-gray-300 rounded-lg"/>
+      <input type="text" placeholder="Password" className="mt-2 p-2 bg-blue-600 text-white rounded-lg"/>
+
+      <div className="flex space-x-4 mt-4">
+        <Link to="/dashboard">
           <button className="px-4 py-2 bg-blue-600 text-white rounded-lg">Login</button>
         </Link>
         <Link to="/register">
