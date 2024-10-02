8319a975c7e8ea5a63b6c3e23c8c28b0606e7be0
Mark Anthony <touya1313@hotmail.com>
2024-10-01 18:34:34 -0600

    added tailwind css into login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,16 @@
 // src/components/Login.jsx
 const Login = () => {
-    return <h1>Welcome to GiveEasy!</h1>;
-  };
-  
-  export default Login;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-2xl font-bold text-blue-800">Login to GiveEasy!</h1>
+      <input type="text" placeholder="Username" className="mt-4 p-2 border border-gray-300 rounded-lg"/>
+      <input type="text" placeholder="Password" className="mt-2 p-2 bg-blue-600 text-white rounded-lg"/>
+      <button className="mt-4 p-2 bg-blue-600 text-white rounded-lg">
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Login;
   
