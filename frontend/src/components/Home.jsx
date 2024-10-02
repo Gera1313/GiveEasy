5f99e94cbb93a3a49c2f88a0331081a112c506f2
Mark Anthony <touya1313@hotmail.com>
2024-10-01 18:11:20 -0600

    added some basic tailwind css into home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,11 @@
 // src/components/Home.jsx
 const Home = () => {
-    return <h1>Welcome to GiveEasy!</h1>;
-  };
-  
-  export default Home;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to GiveEasy!</h1>
+    </div>
+  );
+};
+
+export default Home;
   
