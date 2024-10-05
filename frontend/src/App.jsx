508b8cedf5079117059e43e0254d35030cc96ea0
Mark Anthony <touya1313@hotmail.com>
2024-10-05 15:50:43 -0600

    import files in home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 
@@ -9,7 +8,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
   return (
