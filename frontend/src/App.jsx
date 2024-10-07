2e726c89934ce17ada3f7203565a401e3f185500
Mark Anthony <touya1313@hotmail.com>
2024-10-07 07:08:03 -0600

    created CreateFundraiser.jsx and added to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
+import CreateFundraiser from './components/CreateFundraiser';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-fundraiser" element={<CreateFundraiser />} />
       </Routes>
     </Router>
   );
